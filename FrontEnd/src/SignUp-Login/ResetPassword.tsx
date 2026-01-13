03f958ff88c4c0c3db738d9c942910182f2e5d6f
AnishHegde12 <anishghegde@gmail.com>
2026-01-13 17:31:30 +0530

    Updated ResetPassword.tsx to include a countdown timer for resending OTPs.

--- FrontEnd/src/SignUp-Login/ResetPassword.tsx
+++ FrontEnd/src/SignUp-Login/ResetPassword.tsx
@@ -1,9 +1,10 @@
 import { useState } from "react"
 import { changePassword, sendOtp, verifyOtp } from "../Services/UserService"
-import { Modal,TextInput,Button,PinInput,PasswordInput} from '@mantine/core';
+import { Modal, TextInput, Button, PinInput, PasswordInput } from '@mantine/core';
 import { IconAt, IconLock } from "@tabler/icons-react";
 import { signupValidation } from "../Services/formValidation";
 import { errorNotification, successNotification } from "../Services/NotificationService";
+import { useInterval } from "@mantine/hooks/lib/use-interval/use-interval";
 
 const ResetPassword = (props: any) => {
 
@@ -13,12 +14,25 @@ const ResetPassword = (props: any) => {
     const [otpsent, setOtpSent] = useState(false)
     const [otpSending, setOtpSending] = useState(false)
     const [otpVerified, setOtpVerified] = useState(false)
+    const [resendLoader, setResendLoader] = useState(false)
+    const [seconds, setSeconds] = useState(60);
+    const interval = useInterval(() => {
+        if (seconds === 0) {
+            setResendLoader(false);
+            setSeconds(60);
+            interval.stop();
+            return;
+        }
+        else setSeconds((s) => s - 1)
+    }, 1000);
     const handleSendOtp = () => {
         setOtpSending(true);
         sendOtp(email).then((res) => {
             setOtpSent(true);
             successNotification("OTP Sent Successfully", "Please check your email for the OTP.");
             setOtpSending(false);
+            setResendLoader(true);
+            interval.start();
         }).catch((err) => {
             console.log(err);
             errorNotification("OTP Sending Failed", err.response.data.errorMessage || "Failed to send OTP. Please try again.");
@@ -29,18 +43,24 @@ const ResetPassword = (props: any) => {
         verifyOtp(email, value).then((res) => {
             console.log(res);
             setOtpVerified(true);
-            successNotification("OTP Verified Successfully","Enter your new password below.");
+            successNotification("OTP Verified Successfully", "Enter your new password below.");
         }).catch((err) => {
             console.log(err);
             errorNotification("OTP Verification Failed", err.response.data.errorMessage || "Failed to verify OTP. Please try again.");
         })
     }
     const resendOtp = () => {
+        if (resendLoader) return;
         handleSendOtp();
+
     }
     const changeEmail = () => {
         setOtpSent(false);
         setEmail('');
+        setResendLoader(false);
+        setSeconds(60);
+        setOtpVerified(false);
+        interval.stop();
     }
     const handleResetPassword = () => {
         changePassword(email, password).then((res) => {
@@ -52,7 +72,7 @@ const ResetPassword = (props: any) => {
             errorNotification("Password Reset Failed", err.response.data.errorMessage || "Failed to change password. Please try again.");
         })
     }
-     
+
     return <Modal opened={props.opened} onClose={props.close} title="Reset Password">
         <div className="flex flex-col gap-6">
             <TextInput value={email} onChange={(e) => setEmail(e.target.value)} size="md" name="email" withAsterisk leftSection={<IconAt size={16} />} label="Email" placeholder="Your email"
@@ -61,16 +81,16 @@ const ResetPassword = (props: any) => {
         {otpsent && <PinInput onComplete={handleverifyOtp} length={6} type="number" size="md" gap="lg" className="mx-auto" />}
         {otpsent && !otpVerified &&
             <div className="flex gap-2">
-                <Button fullWidth color="brightSun.4" loading={otpSending} onClick={resendOtp} variant="light" size="xs" autoContrast>Resend OTP</Button>
+                <Button fullWidth color="brightSun.4" loading={otpSending} onClick={resendOtp} variant="light" size="xs" autoContrast>{resendLoader ? seconds : "Resend OTP"}</Button>
                 <Button onClick={changeEmail} variant="light" size="xs" autoContrast>Change Email</Button>
             </div>
         }
         {
-            otpVerified && 
-            <PasswordInput value={ password } error={ passwordError } onChange={ (e) => {setPassword(e.target.value); setPasswordError(signupValidation("password", e.target.value))} } name="password" leftSection={ <IconLock size={ 18 } stroke={ 1.5 } /> } label="Password" withAsterisk placeholder="Enter Password" />
+            otpVerified &&
+            <PasswordInput value={password} error={passwordError} onChange={(e) => { setPassword(e.target.value); setPasswordError(signupValidation("password", e.target.value)) }} name="password" leftSection={<IconLock size={18} stroke={1.5} />} label="Password" withAsterisk placeholder="Enter Password" />
         }
         {
-            otpVerified && 
+            otpVerified &&
             <Button onClick={handleResetPassword} autoContrast variant="filled">Change Password</Button>
         }
     </Modal>
