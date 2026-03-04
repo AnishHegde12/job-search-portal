119b5d57753599d0412672baffe477dcf51425b3
AnishHegde12 <anishghegde@gmail.com>
2026-03-04 22:22:00 +0530

    local storage and slice

--- FrontEnd/src/Services/LocalStorageService.tsx
+++ FrontEnd/src/Services/LocalStorageService.tsx
@@ -1,4 +1,13 @@
 const setItem=(key:string, value:any)=>{
     localStorage.setItem(key,JSON.stringify(value));
 }
-export default setItem;
+
+const getItem=(key:string)=>{
+    return JSON.parse(localStorage.getItem(key) as string);
+}
+
+const removeItem=(key: string)=>{
+    localStorage.removeItem(key);
+}
+
+export {setItem, getItem, removeItem}
---
+++ FrontEnd/src/Slices/UseSlice.tsx
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { getItem, removeItem, setItem } from "../Services/LocalStorageService";
+
+const UserSlice=createSlice({
+    name:"user",
+    initialState:getItem("user"),
+    reducers:{
+        setUser:(state, action)=>{
+        setItem("user", action.payload)
+        state=getItem("user")
+        return state;
+    },
+    removeUser:(state)=>{
+        removeItem("user");
+        state=null;
+        return state;
+    }
+    }
+})
